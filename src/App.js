9011ffe95714cc553493994cb9eb33d6bfc5841b
Stefan Poibrenski <stefan_poibrenski@abv.bg>
2023-01-12 14:48:24 +0200

    feat: add framer motion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import { motion } from "framer-motion"
 
 function App() {
-  return (
-    <div className="App">
-      <form>
-        <h1>Create An Account</h1>
-        <label>
-          Email:
-          <input name="email" type="email" required />
-        </label>
-        <label>
-          Password:
-          <input name="password" type="password" required />
-        </label>
-        <button>Sign Up!</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="App">
+            <motion.form
+                initial={{ x: -500 }}
+                animate={{ x: 0 }}
+                transition={{ ease: "easeOut", duration: 2 }}>
+                <h1>Create An Account</h1>
+                <label>
+                    Email:
+                    <input name="email" type="email" required />
+                </label>
+                <label>
+                    Password:
+                    <input name="password" type="password" required />
+                </label>
+                <button>Sign Up!</button>
+            </motion.form>
+        </div>
+    );
 }
 
 export default App;
